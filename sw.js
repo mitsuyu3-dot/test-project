7cf157dda5552b2f9323d561a04edbb2f42ba130
mitsuyu3-dot <mitsuyu3@gmail.com>
2026-09-21 17:43:55 +0900

    Refresh mobile UI assets past service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "velvet-table-v4";
+const CACHE_NAME = "velvet-table-v5-mobile-layout";
 const APP_SHELL = ["./", "./index.html", "./style.css", "./app.js", "./baccarat-rules.mjs", "./card-renderer.mjs", "./squeeze-geometry.mjs", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
